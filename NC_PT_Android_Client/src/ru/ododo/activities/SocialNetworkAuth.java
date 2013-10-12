60996220915f40441331bab6b39291e7804afa60
ae-krasnov <krasnovandriei@gmail.com>
2013-10-13 02:04:27 +0400

    change

---
+++ NC_PT_Android_Client/src/ru/ododo/activities/SocialNetworkAuth.java
@@ -0,0 +1,28 @@
+package ru.ododo.activities;
+
+
+import nc_project_team.nc_prototypeinterface.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SocialNetworkAuth extends Activity {
+	
+
+	//ключ для интента
+	//private final static String NAME_OF_SOCIAL_NETWORK="ru.ododo.android.client.SOCIALNETWORK";
+	//с помощью какой соц.сети входим
+	//private static String ENTRY_BY_;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setTitle("Быстрый вход");
+		setContentView(R.layout.social_network_auth);
+		//название в виде префикса для соц.сети
+		//ENTRY_BY_=getIntent().getExtras().getString(NAME_OF_SOCIAL_NETWORK);
+
+	}
+
+}
