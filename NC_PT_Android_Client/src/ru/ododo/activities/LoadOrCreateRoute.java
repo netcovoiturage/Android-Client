736bfa6e3ef187838a56697cd5de713a76bcde33
ae-krasnov <krasnovandriei@gmail.com>
2013-11-28 20:56:40 +0400

    add service

--- NC_PT_Android_Client/src/ru/ododo/activities/LoadOrCreateRoute.java
+++ NC_PT_Android_Client/src/ru/ododo/activities/LoadOrCreateRoute.java
@@ -1,7 +1,7 @@
 package ru.ododo.activities;
 
 import ru.ododo.logic.Variables;
-import ru.ododo.logic.androidService;
+import ru.ododo.logic.BackgroundWorkService;
 import ru.ododo.logic.systemstate.SysSinglton;
 import nc_project_team.nc_prototypeinterface.R;
 import android.app.Activity;
@@ -9,14 +9,19 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoadOrCreateRoute extends Activity {
+public class LoadOrCreateRoute extends Activity implements OnClickListener {
 
 
 	String userId;
 	String APIUrl;
+	Button btnShutDownService;
+	boolean stopService;
 	
 	private static ProgressDialog pb;
 	//private static LoadOrCreateRoute mySelfRef;
@@ -31,6 +36,10 @@ public class LoadOrCreateRoute extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.load_or_create_new_route);
 		setTitle("Hello, "+SysSinglton.getInstance().getUserFullName());
+		
+		btnShutDownService=(Button)findViewById(R.id.shutDownService);
+		btnShutDownService.setOnClickListener(this);
+		
 		TextView tvHello=(TextView)findViewById(R.id.tvHello);
 		tvHello.setText("Success!");
 		Log.d(Variables.MY_TAG, "title:"+SysSinglton.getInstance().getUserFullName());
@@ -43,8 +52,26 @@ public class LoadOrCreateRoute extends Activity {
 				EnterSystem.getActiv().finish();
 			if(EnterByVk.getAct()!=null)
 				EnterByVk.getAct().finish();
-			startService(new Intent(this, androidService.class));
+			startService(new Intent(this, BackgroundWorkService.class));
 		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(v.getId()==btnShutDownService.getId()){
+			stopService=true;
+			finish();
+		}
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if(stopService)
+			stopService(new Intent(this, BackgroundWorkService.class));
 	}
 
 
---
+++ NC_PT_Android_Client/src/ru/ododo/logic/BackgroundWorkService.java
@@ -0,0 +1,67 @@
+package ru.ododo.logic;
+
+
+
+
+
+import ru.ododo.activities.LoadOrCreateRoute;
+import android.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class BackgroundWorkService extends Service {
+
+	NotificationManager nm;
+	Notification toStatusBar;
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Log.d(Variables.MY_TAG, "Service create");
+		nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		Log.d(Variables.MY_TAG, "Service start command");
+		
+		Intent activityIntent=new Intent(this, LoadOrCreateRoute.class);
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+		
+		Notification toStatusBar=new NotificationCompat.Builder(this)
+									.setContentText("ODODO.RU")
+									.setContentIntent(pIntent)
+									.setContentText("wait responce from server")
+									.setSmallIcon(R.drawable.ic_menu_compass)
+									.setOngoing(true)
+									.build();
+		
+		nm.notify(100500,toStatusBar);
+
+		return START_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d(Variables.MY_TAG, "Service destroy");
+		nm.cancel(100500);
+	}
+
+	
+
+}
